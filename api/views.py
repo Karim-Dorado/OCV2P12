d67bb75ad487bf6765296bcbc5fc22c15a513012
Karim-Dorado <karimdorado@gmail.com>
2022-11-04 19:50:03 +0100

    update filters

--- api/views.py
+++ api/views.py
@@ -2,41 +2,29 @@ from rest_framework import viewsets
 from .models import Client, Contract, Event
 from .serializers import ClientSerializer, ContractSerializer, EventSerializer
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsSaleContactOrReadOnly, IsSaleEmployeeOrReadOnly, IsSupportEmployeeOrReadOnly
+from .permissions import IsSalesContactOrReadOnly, IsContractSalesContactOrReadOnly, IsSupportContactOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    permission_classes = [IsAuthenticated, IsSaleEmployeeOrReadOnly]
+    permission_classes = [IsAuthenticated, IsSalesContactOrReadOnly]
     filter_backends = [DjangoFilterBackend]
 
-    filterset_fields = ['id',
-                        'first_name',
+    filterset_fields = ['first_name',
                         'last_name',
-                        'email',
-                        'phone',
-                        'mobile',
-                        'company_name',
-                        'created_at',
-                        'updated_at',
-                        'date_revokated']
+                        'email']
 
 
 class ContractViewSet(viewsets.ModelViewSet):
     serializer_class = ContractSerializer
-    permission_classes = [IsAuthenticated, IsSaleContactOrReadOnly]
+    permission_classes = [IsAuthenticated, IsContractSalesContactOrReadOnly]
     filter_backends = [DjangoFilterBackend]
 
-    filterset_fields = ['id',
-                        'client',
-                        'sales_contact',
+    filterset_fields = ['client',
                         'date_signed',
-                        'status',
-                        'amount',
-                        'payement_due',
-                        'date_revokated']
+                        'amount']
 
     def get_queryset(self):
         return Contract.objects.filter(client=self.kwargs['client_pk'])
@@ -44,20 +32,12 @@ class ContractViewSet(viewsets.ModelViewSet):
 
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated, IsSupportEmployeeOrReadOnly]
+    permission_classes = [IsAuthenticated, IsSupportContactOrReadOnly]
     filter_backends = [DjangoFilterBackend]
 
-    filterset_fields = ['id',
-                        'name',
-                        'contract',
-                        'support_contact',
-                        'attendees',
+    filterset_fields = ['name',
                         'start_date',
-                        'end_date',
-                        'notes',
-                        'created_at',
-                        'updated_at',
-                        'date_revokated']
+                        'end_date']
 
     def get_queryset(self):
         return Event.objects.filter(contract=self.kwargs['contract_pk'])
