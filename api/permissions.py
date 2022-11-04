350079cbc1ffeed03e4c840d998315ef86ed63ec
Karim-Dorado <karimdorado@gmail.com>
2022-11-04 19:48:05 +0100

    update permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,24 +1,7 @@
 from rest_framework import permissions
 
 
-class IsSaleEmployeeOrReadOnly(permissions.BasePermission):
-    message = "Only a sale employee assigned to the client or a manager can make changes"
-
-    def has_permission(self, request, view):
-        if request.method == 'POST' and request.user.department == 'support':
-            return False
-        return True
-
-    def has_object_permission(self, request, view, obj):
-
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return (request.user.department == 'sales'
-                or request.user.department == 'management'
-                )
-
-class IsSaleContactOrReadOnly(permissions.BasePermission):
+class IsSalesContactOrReadOnly(permissions.BasePermission):
     message = "Only a sale employee assigned to the client or a manager can make changes"
 
     def has_permission(self, request, view):
@@ -36,7 +19,25 @@ class IsSaleContactOrReadOnly(permissions.BasePermission):
                 )
 
 
-class IsSupportEmployeeOrReadOnly(permissions.BasePermission):
+class IsContractSalesContactOrReadOnly(permissions.BasePermission):
+    message = "Only a sale employee assigned to the client or a manager can make changes"
+
+    def has_permission(self, request, view):
+        if request.method == 'POST' and request.user.department == 'support':
+            return False
+        return True
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (obj.client.sales_contact == request.user
+                or request.user.department == 'management'
+                )
+
+
+class IsSupportContactOrReadOnly(permissions.BasePermission):
     message = "Only a sale/support employee assigned to the client or a manager can make changes"
 
     def has_permission(self, request, view):
@@ -49,6 +50,6 @@ class IsSupportEmployeeOrReadOnly(permissions.BasePermission):
             return True
 
         return (obj.support_contact == request.user
-                or obj.contract.sales_contact == request.user
+                or obj.contract.client.sales_contact == request.user
                 or request.user.department == 'management'
                 )
